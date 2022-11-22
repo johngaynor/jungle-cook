79d612d6d5f14501db334586b8f091543d2834f4
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-11-22 13:49:57 -0500

    init commit

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,7 @@
+var _db = "";
 var userExists = false;
 var userDisplayName = "";
+var _userProfileInfo = "";
 
 function changeRoute() {
   let hashTag = window.location.hash;
@@ -22,6 +24,48 @@ function initURLListener() {
   $(window).on("hashchange", changeRoute);
   changeRoute();
   $("#navSignOut").hide();
+  $("#navYourRecipes").hide();
+}
+
+function loadLists() {
+  let listString = "<ul>";
+  $.each(_userProfileInfo.lists, function (idx, list) {
+    listString += `<p>list stuff #${idx}, other stuff ${list.listItems.length}</p>`;
+  });
+  listString += "</ul>";
+  $("#app").append(listString);
+}
+
+// loadListItems => just change LISTS to _userProfileInfo.lists[]
+
+function addMainList() {
+  let newListName = $("#listName").val();
+  let newListObj = {
+    name: newListName,
+    listItems: [],
+  };
+
+  _userProfileInfo.lists.push(newListObj);
+  updateUserInfo(_userProfileInfo);
+  loadLists();
+  $("#listName").val("");
+}
+
+function updateUserInfo(userObj) {
+  let id = firebase.auth().currentUser.uid;
+  // if theres a user logged in, it will get the uid
+  _db
+    .collection("Users")
+    .doc(id)
+    .update(userObj)
+    .then(() => {
+      console.log("updated doc");
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("update error " + errorMessage);
+    });
 }
 
 function navListeners() {
@@ -36,22 +80,28 @@ function navListeners() {
   });
 }
 
-function intiFirebase() {
+function initFirebase() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
+      _db = firebase.firestore();
       console.log("app.js > line 36 > auth change logged in");
-      // if (user.displayName) {
-      //   $(".create-header-name").html(displayName);
-      // }
       userExists = true;
       if (user.displayName) {
         $(".nav-links").append(`<div id="nav-fName">${user.displayName}</div>`);
-        $("#header-fName").html("new");
       }
     } else {
+      _db = "";
+      _userProfileInfo = "";
       console.log("app.js > line 38 > auth change logged out");
       userExists = false;
       displayName = "";
+      $("#navCreate").click(function () {
+        console.log("clicked create");
+
+        setTimeout(function () {
+          location.href = "#login";
+        }, 50);
+      });
     }
   });
 }
@@ -62,6 +112,12 @@ function createAccount() {
   let email = $("#signup-email").val();
   let password = $("#signup-password").val();
   let fullName = fName + " " + lName;
+  let userObj = {
+    firstName: fName,
+    lastName: lName,
+    email: email,
+    lists: [],
+  };
   console.log("create " + fName + " " + lName);
 
   firebase
@@ -74,6 +130,23 @@ function createAccount() {
       firebase.auth().currentUser.updateProfile({
         displayName: fName,
       });
+
+      _db
+        .collection("Users")
+        .doc(user.uid)
+        .set(userObj)
+        .then(() => {
+          console.log("doc added");
+          _userProfileInfo = userObj;
+          console.log("create userInfo ", _userProfileInfo);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log("create error " + errorMessage);
+          // ..
+        });
+
       userDisplayName = fName;
       $("#fname").val("");
       $("#lname").val("");
@@ -81,6 +154,7 @@ function createAccount() {
       $("#signup-password").val("");
       $("#navLogin").hide();
       $("#navSignOut").show();
+      $("#navYourRecipes").show();
       $(".nav-links").append(`<div id="nav-fName">${userDisplayName}</div>`);
       // $("#nav-fName").html(fName);
       // $.get(`pages/your-recipes/your-recipes.html`, function (data) {
@@ -95,6 +169,26 @@ function createAccount() {
     });
 }
 
+function updateUserContent() {
+  location.href = "#create";
+  // console.log(window.location.hash);
+  // if (window.location.hash === "#create") {
+  //   $("#app").html();
+  // } else {
+  //   console.log("error loading " + errorMessage);
+  // }
+}
+
+function logInUpdatePage(user) {
+  location.href = "#create";
+  console.log("update page works");
+  setTimeout(function () {
+    console.log("page loaded correctly");
+    $("#header-fName").html(`hey ${user.displayName}, create your recipe!`);
+  }, 50);
+  // 15ms is the lowest I could go for it to still work, should probably set it higher in case other computers don't load as fast
+}
+
 function logIn() {
   let email = $("#login-email").val();
   let password = $("#login-password").val();
@@ -106,19 +200,16 @@ function logIn() {
       // Signed in
       var user = userCredential.user;
       console.log("logged in");
-      // $("#login-email").val("");
-      // $("#login-password").val("");
       $("#navLogin").hide();
       $("#navSignOut").show();
-      // $("#header-fName").remove();
-      location.href = "#create";
-      // $(".create").html(`hey ${user.displayName}, do stuff`);
+      $("#navYourRecipes").show();
 
-      // ...
+      logInUpdatePage(user);
     })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
+
       console.log("logged in error " + errorMessage);
     });
 }
@@ -147,6 +238,7 @@ function signOut() {
       console.log("signed out");
       $("#navSignOut").hide();
       $("#navLogin").show();
+      $("#navYourRecipes").hide();
       $("#nav-fName").remove();
     })
     .catch((error) => {
@@ -157,7 +249,7 @@ function signOut() {
 $(document).ready(function () {
   try {
     let app = firebase.app();
-    intiFirebase();
+    initFirebase();
     // signInAnon();
   } catch (error) {
     console.log("error ", error);
@@ -165,3 +257,11 @@ $(document).ready(function () {
   initURLListener();
   navListeners();
 });
+
+// function userUpdateContent() {
+//   $(document)
+//     .then(function () {
+//       $("#header-fName").html();
+//     })
+//     .catch(console.log("loading error"));
+// }
