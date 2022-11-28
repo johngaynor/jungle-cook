710d87352868ca05e76c0ddd57ecb0b4f8957fa0
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-11-28 12:07:39 -0500

    delete btn works

--- public/app/app.js
+++ public/app/app.js
@@ -93,32 +93,7 @@ var userExists = false;
 var userDisplayName = "";
 var _userProfileInfo = {};
 
-function loadDefaultRecipes() {
-  $.each(RECIPES, function (index, recipe) {
-    $(".recipes-container").append(`<div class="recipe-box" id="${index}">
-    <div class="recipe-img">
-    <img src="images/${recipe.recipeImage}" alt="" />
-    </div>
-    <div class="recipe-desc">
-      <h1>${recipe.recipeName}</h1>
-      <p>
-      ${recipe.recipeDesc}
-      </p>
-      <div class="recipe-time">
-        <div class="recipe-time-img"></div>
-        <p>${recipe.recipeTime}</p>
-      </div>
-      <div class="recipe-servings">
-        <div class="recipe-servings-img"></div>
-        <p>${recipe.recipeServings}</p>
-      </div>
-    </div>
-  </div>`);
-
-    loadFullRecipe();
-  });
-}
-
+// this area is for browse.html funtionality //////////////
 function initDefaultRecipeStyling() {
   $("#app").html(`<div class="recipes">
   <div class="recipes-content">
@@ -130,14 +105,14 @@ function initDefaultRecipeStyling() {
 </div>`);
 }
 
-function returnToRecipes() {
+function returnToDefaultRecipes() {
   $("#recipe-full-back").click(function () {
     initDefaultRecipeStyling();
     loadDefaultRecipes();
   });
 }
 
-function loadFullRecipe() {
+function loadFullDefaultRecipe() {
   $(".recipe-box").click(function (e) {
     let recipeIndex = e.currentTarget.id;
     console.log("clicked " + recipeIndex);
@@ -185,9 +160,36 @@ function loadFullRecipe() {
   </div>
   `);
 
-    returnToRecipes();
+    returnToDefaultRecipes();
   });
 }
+
+function loadDefaultRecipes() {
+  $.each(RECIPES, function (index, recipe) {
+    $(".recipes-container").append(`<div class="recipe-box" id="${index}">
+    <div class="recipe-img">
+    <img src="images/${recipe.recipeImage}" alt="" />
+    </div>
+    <div class="recipe-desc">
+      <h1>${recipe.recipeName}</h1>
+      <p>
+      ${recipe.recipeDesc}
+      </p>
+      <div class="recipe-time">
+        <div class="recipe-time-img"></div>
+        <p>${recipe.recipeTime}</p>
+      </div>
+      <div class="recipe-servings">
+        <div class="recipe-servings-img"></div>
+        <p>${recipe.recipeServings}</p>
+      </div>
+    </div>
+  </div>`);
+
+    loadFullDefaultRecipe();
+  });
+}
+///////////////////////////////////////////////////////////
 
 function loadYourRecipeFull() {
   $(".your-recipe-box").click(function (e) {
@@ -239,8 +241,7 @@ function loadYourRecipeFull() {
   });
 }
 
-// how to get it to refresh the scroll position when reloading the content?
-
+// this area is for url navigation & nav functionality ////
 function changeRoute() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
@@ -270,16 +271,218 @@ function initURLListener() {
   $("#navYourRecipes").hide();
 }
 
-// function loadLists() {
-//   let listString = "<ul>";
-//   $.each(_userProfileInfo.lists, function (idx, list) {
-//     listString += `<p>list stuff #${idx}, other stuff ${list.listItems.length}</p>`;
-//   });
-//   listString += "</ul>";
-//   $("#app").append(listString);
-// }
+function navListeners() {
+  $(".nav-bars").click(function (e) {
+    $(".nav-bars").toggleClass("active");
+    $(".nav-links").toggleClass("active");
+  });
 
-// loadListItems => just change LISTS to _userProfileInfo.lists[]
+  $(".nav-links a").click(function (e) {
+    $(".nav-bars").removeClass("active");
+    $(".nav-links").removeClass("active");
+  });
+}
+///////////////////////////////////////////////////////////
+
+// this area is for firebase login/signup /////////////////
+function initFirebase() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      _db = firebase.firestore();
+      console.log(">> auth change logged in");
+      userExists = true;
+      if (user.displayName) {
+        $("#navCreate").click(function () {
+          console.log("clicked create logged in");
+
+          setTimeout(function () {
+            location.href = "#create";
+
+            setTimeout(function () {
+              $("#header-fName").html(
+                `hey ${user.displayName}, create your recipe!`
+              );
+            }, 50);
+          }, 50);
+        });
+
+        $("#navYourRecipes").click(function () {
+          // console.log("clicked your-recipes");
+          setTimeout(function () {
+            $("#your-recipes-header").html(
+              `hey ${user.displayName}, here are your recipes!`
+            );
+          }, 50);
+        });
+      }
+    } else {
+      _db = "";
+      _userProfileInfo = {};
+      console.log(">> auth change logged out");
+      userExists = false;
+      displayName = "";
+      $("#navCreate").click(function () {
+        console.log("clicked create logged out");
+
+        setTimeout(function () {
+          location.href = "#login";
+        }, 50);
+      });
+    }
+  });
+}
+
+function logIn() {
+  let email = $("#login-email").val();
+  let password = $("#login-password").val();
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("logged in");
+      $("#navLogin").hide();
+      $("#navSignOut").show();
+      $("#navYourRecipes").show();
+      _db
+        .collection("Users")
+        .doc(user.uid)
+        .get()
+        .then((doc) => {
+          _userProfileInfo = doc.data();
+          console.log("login userInfo ", _userProfileInfo);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+
+          console.log("user data retrieval error " + errorMessage);
+        });
+
+      logInUpdatePage(user);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // $(".login-form").append(
+      //   `<p>Login failed: username and/or password is incorrect.</p>`
+      // );
+      alert("Login failed: username and/or password is incorrect.");
+      console.log("logged in error " + errorMessage);
+      $("#login-password").val("");
+      $("#signup-password").val("");
+      $("#signup-email").val("");
+    });
+}
+
+function createAccount() {
+  let fName = $("#fname").val();
+  let lName = $("#lname").val();
+  let email = $("#signup-email").val();
+  let password = $("#signup-password").val();
+  let fullName = fName + " " + lName;
+  let userObj = {
+    firstName: fName,
+    lastName: lName,
+    email: email,
+    recipes: [],
+  };
+  console.log("create " + fName + " " + lName);
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      console.log("created account");
+      firebase.auth().currentUser.updateProfile({
+        displayName: fName,
+      });
+
+      _db
+        .collection("Users")
+        .doc(user.uid)
+        .set(userObj)
+        .then(() => {
+          console.log("doc added");
+          _userProfileInfo = userObj;
+          console.log("create userInfo ", _userProfileInfo);
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log("create error " + errorMessage);
+          // ..
+        });
+
+      userDisplayName = fName;
+      $("#fname").val("");
+      $("#lname").val("");
+      $("#signup-email").val("");
+      $("#signup-password").val("");
+      $("#navLogin").hide();
+      $("#navSignOut").show();
+      $("#navYourRecipes").show();
+      logInUpdatePage(user);
+    })
+
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      alert(
+        "Sign up failed: email is invalid or an account already exists with this email."
+      );
+      console.log("create error " + errorMessage);
+      $("#signup-password").val("");
+      $("#login-password").val("");
+      // ..
+    });
+}
+
+function signOut() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      console.log("signed out");
+      $("#navSignOut").hide();
+      $("#navLogin").show();
+      $("#navYourRecipes").hide();
+      $("#nav-fName").remove();
+    })
+    .catch((error) => {
+      console.log("Error signing out " + error);
+    });
+}
+///////////////////////////////////////////////////////////
+
+function logInUpdatePage(user) {
+  location.href = "#create";
+  console.log("update page works");
+  setTimeout(function () {
+    console.log("page loaded correctly");
+    $("#header-fName").html(`hey ${user.displayName}, create your recipe!`);
+  }, 50);
+  // 15ms is the lowest I could go for it to still work, should probably set it higher in case other computers don't load as fast
+}
+
+function deleteUserRecipe(recipeIndex, index) {
+  // let recipeIndex = e.currentTarget.id;
+  console.log("attempted to delete");
+  console.log(_userProfileInfo.recipes);
+  _userProfileInfo.recipes.splice(index, 1);
+  console.log(_userProfileInfo.recipes);
+  updateUserInfo(_userProfileInfo);
+  $(".your-recipes-container").html("");
+  loadUserRecipes();
+}
+
+function deleteTest() {
+  loadUserRecipes();
+}
 
 function loadUserRecipes() {
   $.each(_userProfileInfo.recipes, function (index, recipe) {
@@ -297,8 +500,13 @@ function loadUserRecipes() {
         >
           View
         </div>
-        <div id="recipe-btn-edit" class="recipe-btn">Edit Recipe</div>
-        <div id="recipe-btn-delete" class="recipe-btn">Delete</div>
+        <div id="recipe-btn-edit"
+        onclick="deleteTest()"
+        class="recipe-btn">Edit Recipe</div>
+        <div
+        id="recipe-btn-delete"
+        onclick="deleteUserRecipe()"
+        class="recipe-btn">Delete</div>
       </div>
       <img src="images/${recipe.recipeImage}.jpg" alt="" />
     </div>
@@ -320,34 +528,7 @@ function loadUserRecipes() {
   });
 }
 
-// function loadUserRecipesTest() {
-//   console.log("loadUserRecipesTest running");
-//   $.each(RECIPES, function (index, recipe) {
-//     $(
-//       ".your-recipes-container"
-//     ).append(`<div class="your-recipe-box" id="${index}">
-//     <div class="recipe-img">
-//     <img src="images/${recipe.recipeImage}" alt="" />
-//     </div>
-//     <div class="recipe-desc">
-//       <h1>${recipe.recipeName}</h1>
-//       <p>
-//       ${recipe.recipeDesc}
-//       </p>
-//       <div class="recipe-time">
-//         <div class="recipe-time-img"></div>
-//         <p>${recipe.recipeTime}</p>
-//       </div>
-//       <div class="recipe-servings">
-//         <div class="recipe-servings-img"></div>
-//         <p>${recipe.recipeServings}</p>
-//       </div>
-//     </div>
-//   </div>`);
-//   });
-// }
-
-function createRecipeSubmit() {
+function createRecipeSubmit(recipeIndex) {
   let newRecipeName = $("#create-name").val();
   let newRecipeDesc = $("#create-description").val();
   let newRecipeTime = $("#create-time").val();
@@ -416,207 +597,6 @@ function updateUserInfo(userObj) {
     });
 }
 
-function navListeners() {
-  $(".nav-bars").click(function (e) {
-    $(".nav-bars").toggleClass("active");
-    $(".nav-links").toggleClass("active");
-  });
-
-  $(".nav-links a").click(function (e) {
-    $(".nav-bars").removeClass("active");
-    $(".nav-links").removeClass("active");
-  });
-}
-
-function initFirebase() {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      _db = firebase.firestore();
-      console.log("app.js > line 36 > auth change logged in");
-      userExists = true;
-      if (user.displayName) {
-        $("#navCreate").click(function () {
-          console.log("clicked create logged in");
-
-          setTimeout(function () {
-            location.href = "#create";
-
-            setTimeout(function () {
-              $("#header-fName").html(
-                `hey ${user.displayName}, create your recipe!`
-              );
-            }, 50);
-          }, 50);
-        });
-
-        $("#navYourRecipes").click(function () {
-          // console.log("clicked your-recipes");
-          setTimeout(function () {
-            $("#your-recipes-header").html(
-              `hey ${user.displayName}, here are your recipes!`
-            );
-          }, 50);
-        });
-      }
-    } else {
-      _db = "";
-      _userProfileInfo = {};
-      console.log("app.js > line 38 > auth change logged out");
-      userExists = false;
-      displayName = "";
-      $("#navCreate").click(function () {
-        console.log("clicked create logged out");
-
-        setTimeout(function () {
-          location.href = "#login";
-        }, 50);
-      });
-    }
-  });
-}
-
-function createAccount() {
-  let fName = $("#fname").val();
-  let lName = $("#lname").val();
-  let email = $("#signup-email").val();
-  let password = $("#signup-password").val();
-  let fullName = fName + " " + lName;
-  let userObj = {
-    firstName: fName,
-    lastName: lName,
-    email: email,
-    recipes: [],
-  };
-  console.log("create " + fName + " " + lName);
-
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      console.log("created account");
-      firebase.auth().currentUser.updateProfile({
-        displayName: fName,
-      });
-
-      _db
-        .collection("Users")
-        .doc(user.uid)
-        .set(userObj)
-        .then(() => {
-          console.log("doc added");
-          _userProfileInfo = userObj;
-          console.log("create userInfo ", _userProfileInfo);
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log("create error " + errorMessage);
-          // ..
-        });
-
-      userDisplayName = fName;
-      $("#fname").val("");
-      $("#lname").val("");
-      $("#signup-email").val("");
-      $("#signup-password").val("");
-      $("#navLogin").hide();
-      $("#navSignOut").show();
-      $("#navYourRecipes").show();
-    })
-
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      $(".signup-form").append(`<p>Sign up failed: email is invalid.</p>`);
-      console.log("create error " + errorMessage);
-      // ..
-    });
-
-  logInUpdatePage(user);
-}
-
-function updateUserContent() {
-  location.href = "#create";
-  // console.log(window.location.hash);
-  // if (window.location.hash === "#create") {
-  //   $("#app").html();
-  // } else {
-  //   console.log("error loading " + errorMessage);
-  // }
-}
-
-function logInUpdatePage(user) {
-  location.href = "#create";
-  console.log("update page works");
-  setTimeout(function () {
-    console.log("page loaded correctly");
-    $("#header-fName").html(`hey ${user.displayName}, create your recipe!`);
-  }, 50);
-  // 15ms is the lowest I could go for it to still work, should probably set it higher in case other computers don't load as fast
-}
-
-function logIn() {
-  let email = $("#login-email").val();
-  let password = $("#login-password").val();
-
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      console.log("logged in");
-      $("#navLogin").hide();
-      $("#navSignOut").show();
-      $("#navYourRecipes").show();
-      _db
-        .collection("Users")
-        .doc(user.uid)
-        .get()
-        .then((doc) => {
-          _userProfileInfo = doc.data();
-          console.log("login userInfo ", _userProfileInfo);
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-
-          console.log("user data retrieval error " + errorMessage);
-        });
-
-      logInUpdatePage(user);
-    })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      $(".login-form").append(
-        `<p>Login failed: username and/or password is incorrect.</p>`
-      );
-
-      console.log("logged in error " + errorMessage);
-    });
-}
-
-// need to figure out an error with too many signin attempts locking you out
-
-function signOut() {
-  firebase
-    .auth()
-    .signOut()
-    .then(() => {
-      console.log("signed out");
-      $("#navSignOut").hide();
-      $("#navLogin").show();
-      $("#navYourRecipes").hide();
-      $("#nav-fName").remove();
-    })
-    .catch((error) => {
-      console.log("Error signing out " + error);
-    });
-}
-
 $(document).ready(function () {
   try {
     let app = firebase.app();
@@ -651,3 +631,9 @@ $(document).ready(function () {
 //   recipeInstFour: "",
 //   recipeInstFive: "",
 // },
+
+// RANDOM NOTES
+
+// how to get it to refresh the scroll position when reloading the content?
+
+// individual error messages for specific errors (i.e. login error for invalid password vs too many failed attempts)
